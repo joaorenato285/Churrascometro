2de4d8ec1e3a84f867fce4fc84ffbfe3d7a67eb6
joaorenato285 <91025365+joaorenato285@users.noreply.github.com>
2021-09-26 19:01:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 let inputAdultos = document.getElementById("adultos");
-let inputCriancas =document.getElementById("criancas");
+let inputCriancas = document.getElementById("criancas");
 let inputHoras = document.getElementById("horas");
 
-let resultado = document.getElementById("resultado")
-let container = document.getElementsByClassName("container")[0]
+let resultado = document.getElementById("resultado");
+let container = document.getElementsByClassName("container")[0];
 
-function calcular(){
+function calcular() {
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let horas = inputHoras.value;
-    
 
-    let carne = adultos * 400 +(criancas*400/2);
-    if(horas>=6){ carne = adultos * 600 +(criancas*600/2);
+
+    let carne = adultos * 400 + (criancas * 400 / 2);
+    if (horas >= 6) {
+        carne = adultos * 600 + (criancas * 600 / 2);
 
     }
 
-    let cerveja = adultos * 1200;
-    if(horas>=6){ cerveja = adultos * 2000;
+    let cerveja = adultos * 2000;
+    if (horas >= 6) {
+        cerveja = adultos * 1800;
     }
 
     let bebidas = criancas * 1000;
-    if(horas>=6){ bebidas = criancas * 1500;
+    if (horas >= 6) {
+        bebidas = criancas * 1500;
     }
-    container.style.height = "40rem";
-    resultado.innerHTML += `<h2 class="h2">Você vai precisar de:</h2>`;
-    resultado.innerHTML += `<p>${carne/1000} Kg de <u>Carne</u></p>`;
-    resultado.innerHTML +=`<p>${Math.ceil (cerveja/350)} Latinhas de <u>Cerveja</u></p>`;
-    resultado.innerHTML += `<p>${Math.ceil (bebidas/2000)} Garrafas de <u>Refrigerante</u></p>`;
+    container.style.height = "42rem";
+    resultado.innerHTML = `<h2>Você vai precisar de:</h2>`
+    resultado.innerHTML += `
+      <div>
+        <img width="35px" src="Assets/carne-removebg-preview.png"/>
+        <p>${carne/1000} Kg de Carne</p>
+      </div>
+    `
+    resultado.innerHTML += `
+      <div>
+        <img width="20px" src="Assets/cerveja.png"/>
+        <p>${Math.ceil(cerveja/355)} latas de Cerveja</p>
+      </div>
+    `
+    resultado.innerHTML += `
+      <div>
+        <img width="20px"  src="Assets/bebida-removebg-preview.png""/>
+        <p>${Math.ceil(bebidas/2000)} garrafas de Bebidas</p>
+      </div>
+     
+    `
 
 }
 
